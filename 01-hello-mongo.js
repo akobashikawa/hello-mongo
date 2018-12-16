3fd8f201286560c0286e34d743f511f63e6a4f27
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-12-16 15:04:53 -0500

    Use collection name as env parameter

--- 01-hello-mongo.js
+++ 01-hello-mongo.js
@@ -4,6 +4,7 @@ console.log(process.env.DB_HOST);
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
 console.log(process.env.DB_NAME);
+console.log(process.env.COLLECTION);
 
 const MongoClient = require('mongodb').MongoClient;
 
