60b426b106bf8964f9e00f6b3f3e6c24140ee120
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-12-16 15:02:28 -0500

    Use DB_NAME as env parameter

--- 01-hello-mongo.js
+++ 01-hello-mongo.js
@@ -3,6 +3,7 @@ console.log(process.env.DB_PROT);
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
+console.log(process.env.DB_NAME);
 
 const MongoClient = require('mongodb').MongoClient;
 
@@ -14,7 +15,7 @@ MongoClient.connect(urlConn, { useNewUrlParser: true }, (err, dbconn) => {
   }
 
   console.log(`Connection: OK`, dbconn);
-  const db = dbconn.db('avivaDev');
+  const db = dbconn.db(`${process.env.DB_NAME}`);
   db.collection('users').find({}).toArray((err, result) => {
     if (err) {
       console.log('users find: KO');
