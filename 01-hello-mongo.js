7d6c645762efc0541dbd86b66afe921b95416716
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-12-16 14:58:53 -0500

    First commit

---
+++ 01-hello-mongo.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+console.log(process.env.DB_PROT);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+const MongoClient = require('mongodb').MongoClient;
+
+const urlConn = `${process.env.DB_PROT}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
+MongoClient.connect(urlConn, { useNewUrlParser: true }, (err, dbconn) => {
+  if (err) {
+    console.log(`Connection: KO`, err);
+    return;
+  }
+
+  console.log(`Connection: OK`, dbconn);
+  const db = dbconn.db('avivaDev');
+  db.collection('users').find({}).toArray((err, result) => {
+    if (err) {
+      console.log('users find: KO');
+      return err;
+    }
+
+    console.log('users find: OK', result);
+  });
+
+  dbconn.close();
+
+  return;
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello Mongo
+
+Learning Mongo.
